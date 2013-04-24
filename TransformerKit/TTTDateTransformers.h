17e3df2835640c6bbcde1eda1455ce6dcbb11c60
Cory D. Wiles <kwylez@gmail.com>
2013-04-24 15:14:31 -0400

    code comments and renamed const

--- TransformerKit/TTTDateTransformers.h
+++ TransformerKit/TTTDateTransformers.h
@@ -9,24 +9,49 @@
 #import <Foundation/Foundation.h>
 
 /**
-
+ * Reference to transformer that will take a date object and return 
+ * a string using the default formatter
+ * @see NSDateFormatter+TransformerKitAdditions for standard format
  */
-extern NSString * const TTTDefaultDateTransformerName;
+extern NSString * const TTTDefaultDateStringTransformerName;
 
 /**
-
+ * Reference to transformer that will take a date object and return
+ * a string using a custom format string
  */
-extern NSString * const TTTDateTransformerName;
+extern NSString * const TTTDateStringTransformerName;
 
 /**
+ * Reference to transformer that will take a date string and return
+ * a date object using the default formatter
+ * @see NSDateFormatter+TransformerKitAdditions for standard format
+ */
+extern NSString * const TTTDefaultStringDateTransformerName;
 
+/**
+ * Reference to transformer that will take a date string and return
+ * a date object using a custom format string
+ */
+extern NSString * const TTTStringDateTransformerName;
+
+/**
+ * If you are going to use a custom formatter than you need to pass then this
+ * is the dictionary key that must be used.
  */
 extern NSString * const TTTDateTransformerCustomFormatKey;
 
 /**
-
+ * If you are going to use a custom formatter than you need to pass then this
+ * is the dictionary key that must be used that references your date object.
  */
-extern NSString * const TTTDateTransformerCustomDateKey;
+extern NSString * const TTTDateTransformerCustomDateStringKey;
+
+/**
+ * If you are going to use a custom formatter than you need to pass then this
+ * is the dictionary key that must be used that references your string 
+ * representation of a date
+ */
+extern NSString * const TTTDateTransformerCustomStringDateKey;
 
 @interface TTTDateTransformers : NSObject
 
