c064303c52eb04e220e539b907477eefeb640035
Mattt Thompson <m@mattt.me>
2013-04-26 13:40:41 -0500

    Adding date transformers (ISO 8601 Timestamp <-> NSDate)

--- Example/main.m
+++ Example/main.m
@@ -1,10 +1,23 @@
+// main.m
+// Copyright (c) 2012 Mattt Thompson (http://mattt.me)
 //
-//  main.m
-//  TransformerKit Example
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
-//  Created by Mattt Thompson on 2012/11/09.
-//  Copyright (c) 2012年 Mattt Thompson. All rights reserved.
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
 //
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 #import <Foundation/Foundation.h>
 
@@ -13,21 +26,27 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         [@{
-         @"Capitalized String" : TTTCapitalizedStringTransformerName,
-         @"Uppercase String" : TTTUppercaseStringTransformerName,
-         @"Lowercase String" : TTTLowercaseStringTransformerName,
-         @"Camel Case String" : TTTCamelCaseStringTransformerName,
-         @"Llama Case String" : TTTLlamaCaseStringTransformerName,
-         @"Snake Case String" : TTTSnakeCaseStringTransformerName,
-         @"Train Case String" : TTTTrainCaseStringTransformerName,
-         @"Reversed String" : TTTReverseStringTransformerName,
-         @"Rémövê Dîaçritics" : TTTRemoveDiacriticStringTransformerName,
-         @"ट्रांस्लितेराते स्ट्रिंग" : TTTTransliterateStringToLatinTransformerName,
-        } enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
-            NSLog(@"%@: %@", key, [[NSValueTransformer valueTransformerForName:obj] transformedValue:key]);
+            @"Capitalized String" : TTTCapitalizedStringTransformerName,
+            @"Uppercase String" : TTTUppercaseStringTransformerName,
+            @"Lowercase String" : TTTLowercaseStringTransformerName,
+            @"Camel Case String" : TTTCamelCaseStringTransformerName,
+            @"Llama Case String" : TTTLlamaCaseStringTransformerName,
+            @"Snake Case String" : TTTSnakeCaseStringTransformerName,
+            @"Train Case String" : TTTTrainCaseStringTransformerName,
+            @"Reversed String" : TTTReverseStringTransformerName,
+            @"Rémövê Dîaçritics" : TTTRemoveDiacriticStringTransformerName,
+            @"ट्रांस्लितेराते स्ट्रिंग" : TTTTransliterateStringToLatinTransformerName,
+        } enumerateKeysAndObjectsUsingBlock:^(id value, id name, BOOL *stop) {
+            NSLog(@"%@: %@", value, [[NSValueTransformer valueTransformerForName:name] transformedValue:value]);
+        }];
+
+        [@[TTTISO8601DateTransformerName] enumerateObjectsUsingBlock:^(id name, NSUInteger idx, BOOL *stop) {
+            NSValueTransformer *transformer = [NSValueTransformer valueTransformerForName:name];
+            NSLog(@"%@ (Timestamp): %@", name, [transformer transformedValue:[NSDate date]]);
+            NSLog(@"%@ (Date): %@", name, [transformer reverseTransformedValue:[transformer transformedValue:[NSDate date]]]);
         }];
     }
-    
+
     return 0;
 }
 
---
+++ TransformerKit/TTTDateTransformers.m
@@ -0,0 +1,87 @@
+// TTTDateTransformers.m
+// 
+// Copyright (c) 2013 Mattt Thompson
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#import "TTTDateTransformers.h"
+
+#import "NSValueTransformer+TransformerKit.h"
+
+#include <time.h>
+#include <xlocale.h>
+
+NSString * const TTTISO8601DateTransformerName = @"TTTISO8601DateTransformerName";
+
+static NSString * TTTISO8601TimestampFromDate(NSDate *date) {
+    static NSDateFormatter *_iso8601DateFormatter = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _iso8601DateFormatter = [[NSDateFormatter alloc] initWithDateFormat:@"%Y-%m-%dT%H:%M:%S%z" allowNaturalLanguage:NO];
+        [_iso8601DateFormatter setLocale:[[NSLocale alloc] initWithLocaleIdentifier:@"en_US_POSIX"]];
+    });
+
+    return [_iso8601DateFormatter stringFromDate:date];
+}
+
+static NSDate * TTTDateFromISO8601Timestamp(NSString *timestamp) {
+    static int const ISO_8601_MAX_LENGTH = 25;
+    
+    const char *source = [timestamp cStringUsingEncoding:NSUTF8StringEncoding];
+    char destination[ISO_8601_MAX_LENGTH];
+    size_t length = strlen(source);
+    
+    if (length == 0) {
+        return nil;
+    }
+
+    if (length == 20 && source[length - 1] == 'Z') {
+        memcpy(destination, source, length - 1);
+        strncpy(destination + length - 1, "+0000\0", 6);
+    } else if (length == 25 && source[22] == ':') {
+        memcpy(destination, source, 22);
+        memcpy(destination + 22, source + 23, 2);
+    } else {
+        memcpy(destination, source, MIN(length, ISO_8601_MAX_LENGTH - 1));
+    }
+    
+    destination[sizeof(destination) - 1] = NULL;
+
+    struct tm time = {
+        .tm_isdst = -1,
+    };
+
+    strptime_l(source, "%FT%T%z", &time, NULL);
+
+    return [NSDate dateWithTimeIntervalSince1970:mktime(&time)];
+}
+
+@implementation TTTDateTransformers
+
++ (void)load {
+    @autoreleasepool {
+        [NSValueTransformer registerValueTransformerWithName:TTTISO8601DateTransformerName transformedValueClass:[NSDate class] returningTransformedValueWithBlock:^id(id value) {
+            return TTTISO8601TimestampFromDate(value);
+        } allowingReverseTransformationWithBlock:^id(id value) {
+            return TTTDateFromISO8601Timestamp(value);
+        }];
+    }
+}
+
+@end
